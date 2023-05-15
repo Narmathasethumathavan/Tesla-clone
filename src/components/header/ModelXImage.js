86e71734939e8ade0f7c3628a788acec85866710
Narmatha Sethumathavan <sethurengaraju@gmail.com>
2023-05-15 14:10:56 +0530

    Created ModelX comp.

---
+++ src/components/header/ModelXImage.js
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react';
+const ModelXImage = () =>
+	<div>
+		<img
+			src="https://digitalassets.tesla.com/tesla-contents/image/upload/h_1800,w_2880,c_fit,f_auto,q_auto:best/Model-X-Main-Hero-Desktop-LHD"
+			alt="fs"
+			width="100%"
+			height="50%"
+		/>
+	</div>;
+
+export default ModelXImage;
